ac2d522ad910c546f31ff179493b8f96f1be61d3
EVERTHON MOREIRA CARDOSO <00846712032@senacrs.edu.br>
2026-03-30 21:23:57 -0300

    modulos

--- main.cpp
+++ main.cpp
@@ -2,18 +2,11 @@
 
 int main(){
 
-    int a;
-    int b;
-    //int a, b;
-
-    std::cout<<"Digite um valor para A: "<<std::endl;
-    std::cin>>a;
-
-    std::cout<<"Digite um valor para B: "<<std::endl;
-    std::cin>>b;
-
-    std::cout<<(a != b)<<std::endl;
-    std::cout<<(a == b)<<std::endl;
+    int a = 8;
+    int b = 3;
+    
+    std::cout<<(a % b)<<std::endl;
+   
     
     
     
