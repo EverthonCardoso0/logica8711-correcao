6f4ea4c3acc39ff6bb8424adf31ae85373c51517
EVERTHON MOREIRA CARDOSO <00846712032@senacrs.edu.br>
2026-03-30 21:44:12 -0300

    matematica ++a

--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 
 int main(){
 
-    int a = 8;
-    int b = 3;
+    int a = 10;
     
-    std::cout<<(a % b)<<std::endl;
+    std::cout<<++a<<std::endl;
+    std::cout<<++a<<std::endl;
+    std::cout<<++a<<std::endl;
+    std::cout<<++a<<std::endl;
+
    
     
     
