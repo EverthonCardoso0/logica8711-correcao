d821fbdb507b822251b2ccb5bad876d4ad332617
EVERTHON MOREIRA CARDOSO <00846712032@senacrs.edu.br>
2026-04-01 19:22:18 -0300

    verdade ou falso

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 
 int main(){
 
-    int a = 10;
-    int b = 6;
+    bool condicao1 = true;
+    bool condicao = false;
 
-    std::cout<<((a < b) || (a - b) >= 16)<<std::endl;
-
+    std::cout<<condicao<<std::endl;
+    std::cout<<condicao1<<std::endl;
+    
            
     return 0;
 }
