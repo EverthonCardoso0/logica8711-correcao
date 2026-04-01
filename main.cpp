0910c80116a56ba0277533de1642a1c2bd7f9afd
EVERTHON MOREIRA CARDOSO <00846712032@senacrs.edu.br>
2026-04-01 20:52:26 -0300

    Teste Usuario

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include<iostream>
+#include<string>
 
 int main(){
-
-    auto num1 = 10;
-    auto num2 = 15.80f;
-    auto letra = 'E';
-    auto texto = "Everthon";
     
-    std::cout<<num1<<std::endl;
-    std::cout<<num2<<std::endl;
-    std::cout<<letra<<std::endl;
-    std::cout<<texto<<std::endl;
+    std::string nome;
+    int idade;
+    int anosAtual = 2026;
+   
+
+    std::cout<<"Qual seu nome: "<<std::endl;
+    std::cin>>nome;
+
+    std::cout<<"Qual Sua idade "<<std::endl;
+    std::cin>>idade;
+
+    std::cout<<"Bem-vindo(a) "<<nome<<std::endl;
+    std::cout<<"Voce nasceu em "<<anosAtual-idade<<std::endl;
+
+        
     
 
     return 0;
