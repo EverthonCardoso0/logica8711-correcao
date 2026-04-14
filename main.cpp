a9f3ecea6348f9f00ad5a75337795bfb7ccfdd68
EVERTHON MOREIRA CARDOSO <00846712032@senacrs.edu.br>
2026-04-13 21:44:48 -0300

    meu atividade

--- main.cpp
+++ main.cpp
@@ -1,33 +1,37 @@
 #include<iostream>
+#include<string>
 #include<windows.h>
 
 int main(){
 
-    int opcaoPagamento;
-    int qtdParafuso;
-    float valorParafuso = 1.40;
-    float valorTotalParafuso;
-    float desconto;
+std::string nome;
+float altura;
+int idade;
 
-    std::cout<<"Digite a quantidade de parafusos desejada: "<<std::endl;
-    std::cin>>qtdParafuso;
+std::cout<<"Qual seu nome? "<<std::endl;
+std::cin>>nome;
+std::cout<<"Qual é sua altura? "<<std::endl;
+std::cin>>altura;
+std::cout<<"Quantos anos tu tem? "<<std::endl;
+std::cin>>idade;
 
-    valorTotalParafuso = qtdParafuso * valorParafuso;
+if(idade <= 10){
+    std::cout<<"Bem-vindo, "<<nome<<" Você vai turma Infantil. "<<std::endl;
+}else if(idade <= 18){
+    std::cout<<"Bem-vindo, "<<nome<<" Você vai turma adolescente."<<std::endl;
+}else if(idade <= 60){
+    std::cout<<"Bem-vindo, "<<nome<<" Você vai turma Adulto. "<<std::endl;
+}
 
-    std::cout<<"Qual a forma de pagamento: "<<std::endl;
-    std::cout<<"--- 1 - Pix"<<std::endl;
-    std::cout<<"--- 2 - Dinheiro"<<std::endl;
-    std::cout<<"--- 3 - Cartão"<<std::endl;
-    std::cin>>opcaoPagamento;
-
-    if(opcaoPagamento == 1){
-        std::cout<<"Sua compra ficou no valor total de R$: "<<valorTotalParafuso<<std::endl;
-    }else if(opcaoPagamento == 2){
-        desconto = valorParafuso * 0.90;
-        std::cout<<"Sua compra ficou no valor total de R$: "<<desconto<<std::endl;
-    }else{
-        std::cout<<"Sua compra ficou no valor total de R$: "<<valorTotalParafuso<<std::endl;
-    }
 
     return 0;
 }
+
+
+
+//nome
+//altura
+//idade
+//10 = infantil
+//18 = adolescente
+//60 = adulto
