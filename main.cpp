7f6e639b3fe57661145b4bbee2e6a4a3da2c7131
EVERTHON MOREIRA CARDOSO <00846712032@senacrs.edu.br>
2026-04-15 19:15:17 -0300

    Loop repetida

--- main.cpp
+++ main.cpp
@@ -4,34 +4,8 @@
 
 int main(){
 
-std::string nome;
-float altura;
-int idade;
-
-std::cout<<"Qual seu nome? "<<std::endl;
-std::cin>>nome;
-std::cout<<"Qual é sua altura? "<<std::endl;
-std::cin>>altura;
-std::cout<<"Quantos anos tu tem? "<<std::endl;
-std::cin>>idade;
-
-if(idade <= 10){
-    std::cout<<"Bem-vindo, "<<nome<<" Você vai turma Infantil. "<<std::endl;
-}else if(idade <= 18){
-    std::cout<<"Bem-vindo, "<<nome<<" Você vai turma adolescente."<<std::endl;
-}else if(idade <= 60){
-    std::cout<<"Bem-vindo, "<<nome<<" Você vai turma Adulto. "<<std::endl;
-}
-
-
+    for(int i = 1; i <= 10; i++){
+        std::cout<<"Este é um loop!!"<<std::endl;
+    }
     return 0;
 }
-
-
-
-//nome
-//altura
-//idade
-//10 = infantil
-//18 = adolescente
-//60 = adulto
