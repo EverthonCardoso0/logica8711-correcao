37c9d9929aef9fe2ee407c6a6312a8b852d289d3
EVERTHON MOREIRA CARDOSO <00846712032@senacrs.edu.br>
2026-04-15 21:39:04 -0300

    Int num:mynumers

--- main.cpp
+++ main.cpp
@@ -6,12 +6,9 @@
 
 int main(){
 
-    for(int i = 1; i <= 2; ++i){
-        std::cout<<"Externo: "<<i<<std::endl;
-
-        for(int j = 1; j <= 3; ++j){
-            std::cout<<"Interno: "<<j<<std::endl;
-        }
+    int myNumers[5] = {10, 20, 30, 40, 50};
+    for(int num : myNumers){
+        std::cout<<num<<std::endl;
     }
     
     return 0;
