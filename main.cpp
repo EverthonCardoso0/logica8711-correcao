b8dfa57a33b8df0286eef46301bc29fe3aef0210
EVERTHON MOREIRA CARDOSO <00846712032@senacrs.edu.br>
2026-04-20 21:54:13 -0300

    do num

--- main.cpp
+++ main.cpp
@@ -3,13 +3,19 @@
 
     int main(){
 
-        int contagem = 10;
-        while(contagem > 0){
-             std::cout<<contagem<<std::endl;
-             contagem++;   
-        }
+       int num;
 
-        std::cout<<"Feliz ano novo!!"<<std::endl;
+       do{
+            std::cout<<"Digite um valor para num: "<<std::endl;
+            std::cin>>num;
+
+            if(num < 1 || num > 10){
+                std::cout<<"Entrada inválida!!"<<std::endl;
+            }
+       }while(num < 1 || num > 10);{
+            std::cout<<"Obrigado!! Você escolheu o número: "<<num<<std::endl;
+       }
+
         
-    return 0;
+        return 0;
 }
