9615ea23c6d790d143bf794f8e3e3d6520375a00
EVERTHON MOREIRA CARDOSO <00846712032@senacrs.edu.br>
2026-04-28 19:28:54 -0300

    break numero

--- main.cpp
+++ main.cpp
@@ -3,19 +3,14 @@
 
     int main(){
 
-       int num;
-
-       do{
-            std::cout<<"Digite um valor para num: "<<std::endl;
-            std::cin>>num;
-
-            if(num < 1 || num > 10){
-                std::cout<<"Entrada inválida!!"<<std::endl;
-            }
-       }while(num < 1 || num > 10);{
-            std::cout<<"Obrigado!! Você escolheu o número: "<<num<<std::endl;
-       }
-
-        
+     for(int i = 0; i < 10; i++){
+          if(i == 4){
+               break;
+          }
+          std::cout<<i<<std::endl;
+     
+     }
+     
+       
         return 0;
 }
