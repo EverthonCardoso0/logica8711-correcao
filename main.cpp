4fbcd6e5bdb3d0122e96f914be60d392ab4d28dc
EVERTHON MOREIRA CARDOSO <00846712032@senacrs.edu.br>
2026-04-30 20:17:11 -0300

    brlamount choice aprendendo quanto valor dolar e reais

--- main.cpp
+++ main.cpp
@@ -2,27 +2,29 @@
 
 int main(){
 
-     int choice;
-     float temp, converted;
+    //brlAmount: Valor em reais / exchageRate: Taxa de Câmbio / usdAmount: Valor em dolar
+    double briAmount, exchangeRate, usdAmount;
+    int choice; //choice: escolha
 
-     std::cout<<"---- Temperature converter ----"<<std::endl;
-     std::cout<<"1: Celsius to Fahrenheit"<<std::endl;
-     std::cout<<"2: Fahrenheit to Celsius"<<std::endl;
-     std::cout<<"Choose an option (1 or 2): "<<std::endl;
-     std::cin>>choice;
+    std::cout<<"---- Currency Converter: BRL to USD -----"<<std::endl;
 
-     if(choice == 1){
-          std::cout<<"Enter temperature in Celsius: "<<std::endl;
-          std::cin>>temp;
-          converted = (temp * 1.8) + 32;
-          std::cout<<temp<<"C is "<<converted<<" F."<<std::endl;
-     }else if(choice == 2){
-          std::cout<<"Enter temperature in Fahrenheit: "<<std::endl;
-          std::cin>>temp;
-          converted = (temp - 32) / 1.8;
-          std::cout<<temp<<"F is "<<converted<<" C."<<std::endl;
-     }else{
-          std::cout<<"Invalid option! Please restart. "<<std::endl;
-     } 
+    //Entrada da taxa de câmbio (quanto vale 1 dolar hoje)
+    std::cout<<"Enter the current exchange rate(How much is 1 dollar in Reais?): "<<std::endl;
+    std::cin>>exchangeRate;
+
+    //Entrada da guantia em Reais que a pessoa deseja converter
+    std::cout<<"Enter the amount in BRL(R$): "<<std::endl;
+    std::cin>>briAmount;
+
+    //verificado lógica: garante que o valor informado seja positivo
+    if(briAmount > 0){
+          //calcula: divide o montante em reais pela taxa de câmbio
+          usdAmount = briAmount / exchangeRate;
+          std::cout<<"You will have $"<<usdAmount<<" U$ Dollars."<<std::endl;
+    }else{
+          //Mensagem de erro caso o valor seja zero ou negativo
+          std::cout<<"Error: Please enter a valid amount greater than zero."<<std::endl;
+    }
+
         return 0;
 }
