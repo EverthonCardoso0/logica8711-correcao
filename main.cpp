8c51738f65ad6b7f5c340135dabdb337bdc150a4
EVERTHON MOREIRA CARDOSO <00846712032@senacrs.edu.br>
2026-05-12 19:45:11 -0300

    Escolhe numero soma subtração mult div

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 int main(){
 
         int num1, num2, soma, subt, mult;
+        int op;
         float div;
+        
 
         std::cout<<"Digite aqui o valor para número 1: "<<std::endl;
         std::cin>>num1;
@@ -11,15 +13,35 @@ int main(){
         std::cout<<"Digite aqui o valor para número 2: "<<std::endl;
         std::cin>>num2;
 
-        soma = num1 + num2;
-        subt = num1 - num2;
-        mult = num1 * num2;
-        div = num1 / num2;
+        std::cout<<"----- Escolha a operação -----"<<std::endl;
+        std::cout<<"1 - Soma";
+        std::cout<<"2 - Subtração";
+        std::cout<<"3 - Multiplicação";
+        std::cout<<"4 - Divisão";
+        std::cin>>op;
 
-        std::cout<<"O resultado da soma é: "<<soma<<std::endl;
-        std::cout<<"O resultado da subtração é: "<<subt<<std::endl;
-        std::cout<<"O resultado da multiplicação é: "<<mult<<std::endl;
-        std::cout<<"O resultado da divisão é: "<<div<<std::endl;
+        switch(op){
+         case 1:
+                 soma = num1 + num2;
+                 std::cout<<"O resultado da soma é: "<<soma<<std::endl;
+                 break;
+         case 2:
+                subt = num1 - num2;
+                std::cout<<"O resultado da subtração é: "<<subt<<std::endl;
+                break;
+         case 3:
+                mult = num1 * num2;
+                std::cout<<"O resultado da multiplicação é: "<<mult<<std::endl;
+                break;
+         case 4:
+                div = num1 / num2;
+                std::cout<<"O resultado da divisão é: "<<div<<std::endl;
+                break;
+        default:
+                std::cout<<"Você não digitou nenhum valor válido!"<<std::endl;      
+
+        }
+       
 
          return 0;
 }
