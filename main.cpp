32b47162b77afb2b0c424a1e17ed5b49bff46333
EVERTHON MOREIRA CARDOSO <00846712032@senacrs.edu.br>
2026-05-28 20:27:41 -0300

    Fibonacci if

--- main.cpp
+++ main.cpp
@@ -1,17 +1,27 @@
 #include<iostream>
-void contar(int numero){
-        if(numero == 3){
-            std::cout<<"PARE!!\n";
-            return;
-        
+
+int fibonacci(int n){
+
+    std::cout<<"Calculando fib("<< n<<")"<<std::endl;
+
+    if(n == 0){
+        std::cout<<"Fib(0) = 0 (PARADA)"<<std::endl;
+        return 0;
     }
-    std::cout<<numero<<"\n";
+    if(n == 1){
+        std::cout<<"Fib(1) = 1 (PARADA!)"<<std::endl;
+        return 1;
+    }
     
-    contar(numero - 1);
+    std::cout<<"Fib("<<n<<") = Fib("<<(n-1)<<") + FIb("<<(n-2)<<")\n";
+    int resultado = fibonacci(n - 1) + fibonacci(n - 2);
+    std::cout<<"Fib("<< n << ") = "<< resultado << "\n";
+    return resultado;
 }
+
 int main(){
 
-    contar(5);
+    fibonacci(4);
 }
 
       
