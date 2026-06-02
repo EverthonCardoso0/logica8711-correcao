350b30c09faa791a97798c93f550612380444890
EVERTHON MOREIRA CARDOSO <00846712032@senacrs.edu.br>
2026-06-02 20:24:13 -0300

    int e &int

--- main.cpp
+++ main.cpp
@@ -1,32 +1,18 @@
 #include<iostream>
 #include<string>
 
-bool ehPalindromo(std:: string s, int inicio, int fim){
-    if(inicio >= fim){
-        return true;
-    }
-    if(s[inicio] != s[fim]){
-        return false;
-    }
-    return ehPalindromo(s, inicio + 1, fim -1);
-}
-
 int main(){
 
+    int num1 = 42;
+    int num2 = 35;
+    int idade = 24;
 
-    std::string palavra;
-
-    std::cout<<"Digite um palavra: "<<std::endl;
-    std::cin>>palavra;
-
-    for(char &c : palavra){
-        c = std::tolower(c);
-    }
-    
-    if(ehPalindromo(palavra, 0, palavra.length() - 1)){
-        std::cout<<"É Palidromo!"<<std::endl;
-    }else{
-        std::cout<<"Não é Palindromo!"<<std::endl;
+    std::cout<<"Número: "<<num1<<std::endl;
+    std::cout<<"Endereço: "<<&num1<<std::endl;
+    std::cout<<"Número: "<<num2<<std::endl;
+    std::cout<<"Endereço: "<<&num2<<std::endl;
+    std::cout<<"Número/Idade: "<<idade<<std::endl;
+    std::cout<<"Endereço: "<<&idade<<std::endl;
            
     return 0;
 
