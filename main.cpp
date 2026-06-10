b4cdec26bb3d8f571494af91cb48041a8b497920
EVERTHON MOREIRA CARDOSO <00846712032@senacrs.edu.br>
2026-06-09 21:46:10 -0300

    Aprendendo usar a STRUCT

--- main.cpp
+++ main.cpp
@@ -1,29 +1,90 @@
 #include<iostream>
 #include<string>
 
-struct Pessoa{
-    std::string nome;
-    int idade;
-    float altura;
+struct Tarefa{
+    int id;
+    std::string descricao;
+    bool concluida;
 };
-int main(){
 
-    Pessoa p1;
+Tarefa tarefas[50];
+int totalTarefas = 0;
 
-    p1.nome = "Everthon";
-    p1.idade = 24;
-    p1.altura = 1.75;
+void adicionar(){
+    std::cout<<"\n --- Adicionar tarefas ---"<<std::endl;
 
-    std::cout<<"Nome: "<<p1.nome<<std::endl;
-    std::cout<<"Idade: "<<p1.idade<<std::endl;
-    std::cout<<"Altura: "<<p1.altura<<std::endl;
+    std::cout<<"ID: ";
+    std::cin>>tarefas[totalTarefas].id;
 
-    return 0;
+    std::cin.ignore();
+    std::cout<<"Descrição: ";
+    std::getline(std::cin, tarefas[totalTarefas].descricao);
 
+    tarefas[totalTarefas].concluida = false;
+
+    totalTarefas++;
+    std::cout<<"Tarefa adiciona!"<<std::endl;
 }
 
+void listar(){
+    if(totalTarefas == 0){
+        std::cout<<"\nNenhuma tarefa!"<<std::endl;
+        return;
+    }
+    std::cout<<"\n--- Tarefas ---"<<std::endl;
+    for(int i = 0; i < totalTarefas; i++){
+        std::string status = tarefas[i].concluida ? "S" : "N";
+        std::cout<<"["<<status<<"]"<<tarefas[i].id<<" - "<<tarefas[i].descricao<<std::endl;
+    }
+}
 
+void marcarConcluida(){
+    int idBuscado;
+    std::cout<<"\nID da tarefas: ";
+    std::cin>>idBuscado;
 
+    for(int i = 0; i < totalTarefas; i++){
+        if(tarefas[i].id == idBuscado){
+            tarefas[i].concluida = true;
+            std::cout<<"Marcada como concluida!"<<std::endl;
+            return;
+        }
+    }
+    std::cout<<"Tarefa não encontrada!"<<std::endl;
+}
 
-     
-     
+void menu(){
+    std::cout<<"\n === TODO LIST ==="<<std::endl;
+    std::cout<<"1. Adicionar"<<std::endl;
+    std::cout<<"2. Listar"<<std::endl;
+    std::cout<<"3. Marcar concluida"<<std::endl;
+    std::cout<<"4. Sair"<<std::endl;
+    std::cout<<"Escolha: "<<std::endl;
+}
+
+int main(){
+    int opcao;
+
+    while(true){
+        menu();
+        std::cin>>opcao;
+
+        switch(opcao){
+            case 1:
+                adicionar();
+                break;
+            case 2:
+                listar();
+                break;
+            case 3:
+                marcarConcluida();
+                break;
+            case 4:
+                std::cout<<"Até logo!"<<std::endl;
+                return 0;
+            default:
+                std::cout<<"Opção inválida!"<<std::endl;
+        }
+    }
+    return 0;
+}
