2b82af75a5e123a6a41fd62bf2548a50a811e8dc
EVERTHON MOREIRA CARDOSO <00846712032@senacrs.edu.br>
2026-06-17 20:55:27 -0300

    CAMPEONATOCOPA DO ANO

--- main.cpp
+++ main.cpp
@@ -1,39 +1,26 @@
 #include<iostream>
-#include<string>
+
+struct CampeonatoCopa{
+    int ano;
+    std::string campeao;
+};
 
 int main(){
-    std::string time1, time2;
-    int gols1, gols2;
+    CampeonatoCopa copa[5] = {
+        {2022, "Argentina"},
+        {2018, "França"},
+        {2014, "Alemanha"},
+        {2010, "Espanha"},
+        {2006, "Itália"}
+    };
 
-    std::cout<<"==== COMPRADOR DE PLACAR ===="<<std::endl;
+    std::cout<<"==== Campeões da Copa ===="<<std::endl;
     std::cout<<std::endl;
 
-    std::cout<<"Time 1: ";
-    std::cin>>time1;
-    std::cout<<"Gols time 1: ";
-    std::cin>>gols1;
-
-    std::cout<<"Time 2: ";
-    std::cin>>time2;
-    std::cout<<"Gols time 2: ";
-    std::cin>>gols2;
-
-    std::cout<<std::endl;
-    std::cout<<"==== RESULTADO ===="<<std::endl;
-    std::cout<<time1<<" "<<gols1<<" x "<<gols2<<" "<<time2<<std::endl;
-    std::cout<<std::endl;
-
-    if (gols1 > gols2){
-        std::cout<<"Vencedor: "<<time1<<std::endl;
-        std::cout<<"Perdedor: "<<time2<<std::endl;
+    for(int i = 0; i < 5; i++){
+        std::cout<<copa[i].ano<<" - Campeão: "<<copa[i].campeao<<std::endl;
+        
     }
-    else if (gols2 > gols1){
-        std::cout<<"Vencedor: "<<time2<<std::endl;
-        std::cout<<"Perdedor: "<<time1<<std::endl;
-    }else {
-        std::cout<<"Empate!"<<std::endl;
-    }
-
     return 0;
 }
-    
+
