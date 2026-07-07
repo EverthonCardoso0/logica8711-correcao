85297d3515b5f19f88415517be1f567a26434056
EVERTHON MOREIRA CARDOSO <00846712032@senacrs.edu.br>
2026-07-07 19:19:19 -0300

    EXERCICIO NUMERO SOMA E VECTOR

--- main.cpp
+++ main.cpp
@@ -11,30 +11,26 @@ int main(){
 
    std::cout<<"==== Maior e Menor ===="<<std::endl;
 
-   numeros.push_back(15);
-   numeros.push_back(0);
-   numeros.push_back(25);
-   numeros.push_back(3);
-   numeros.push_back(42);
+   numeros.push_back(10);
+   numeros.push_back(20);
+   numeros.push_back(30);
+   numeros.push_back(40);
 
    std::cout<<"Vector: ";
    for(int i = 0; i < numeros.size(); i++){
     std::cout<<numeros[i]<<" ";
    }
    std::cout<<std::endl;
-   int maior = numeros[0];
-   int menor = numeros[0];
+   int soma = 0;
+  
 
    for(int i = 0; i < numeros.size(); i++){
-    if(numeros[i] > maior){
-        maior = numeros[i];
-    }
-    if(numeros[i] < menor){
-        menor = numeros[i];
-    }
+    soma += numeros[i];
    }
-   std::cout<<"Maior: "<<maior<<std::endl;
-   std::cout<<"Menor: "<<menor<<std::endl;
+   std::cout<<"Soma: "<<soma<<std::endl;
 
     return 0;
 }
+
+
+
