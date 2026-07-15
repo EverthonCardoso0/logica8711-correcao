90f2ad024a1cf6b832799b7a0885d292dda7b7a2
EVERTHON MOREIRA CARDOSO <00846712032@senacrs.edu.br>
2026-07-14 21:28:17 -0300

    Some todos números de 1 a 100

--- main.cpp
+++ main.cpp
@@ -1,22 +1,21 @@
 #include<Windows.h> 
-
 #include <iostream> 
-
- 
-
-using namespace std; 
-
  
 int main(){ 
 
     SetConsoleOutputCP(CP_UTF8); 
-    SetConsoleCP(CP_UTF8); 
-    const float PI = 3.14; 
-    float raio, area; 
- 
-    std::cout <<"Digite o raio do circulo: "<<std::endl; 
-    std::cin>>raio; 
-    area = PI * (raio * raio); 
+    SetConsoleCP(CP_UTF8);
 
+    int soma = 0;
+
+    for(int i = 1; i <= 100; i++){
+      soma += i;
+    
+    }
+    
+    std::cout<<"A Soma numero 1 a 100 é: "<<soma<<std::endl;
+  
+    return 0;
+}
 
  
