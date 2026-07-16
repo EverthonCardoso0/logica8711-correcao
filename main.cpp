b017b059e5152e689106641524ae37a00e47dc60
EVERTHON MOREIRA CARDOSO <00846712032@senacrs.edu.br>
2026-07-16 19:45:11 -0300

    Numeros pares existem 1 a 100 correto

--- main.cpp
+++ main.cpp
@@ -8,12 +8,21 @@ int main(){
 
     std::cout<<"Numeros pares existem de 1 a 100"<<std::endl;
 
-    for(int i = 2; i <= 100; i+=2){
-      std::cout<<i<<std::endl;
+  int contador = 0;
+    for(int i = 1; i <= 100; i++){
+      if  (i % 2 == 0){
+        contador++;
+      }
     }
+    std::cout<<"Quantidade de números pares: "<<contador<<std::endl;
     
     return 0;
 
 }
+
+
+    
+
+
 
  
